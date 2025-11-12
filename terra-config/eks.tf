8511aca35157584eafc0b8d250f5ba007e559fd2
Priya Salunkhe <77096978+Kaustubh-tech17@users.noreply.github.com>
2025-11-12 22:14:58 +0530

    Update eks.tf

--- terra-config/eks.tf
+++ terra-config/eks.tf
@@ -1,18 +1,18 @@
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "Three-tier-cloud"
-  role_arn = aws_iam_role.cluster_role.arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
     subnet_ids = [
-      "subnet-05d3a52cc30950a8c", # us-east-1a
-      "subnet-04b6af886a1fff2a2", # us-east-1b
-      "subnet-05e74108971ae5074", # us-east-1c
-      "subnet-08854e83a1fccda99", # us-east-1d
-      "subnet-097337a08153cd7ef"  # us-east-1f
+      aws_subnet.public[0].id,
+      aws_subnet.public[1].id,
+      aws_subnet.public[2].id
     ]
   }
 
-  depends_on = [
-    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy
-  ]
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy]
+
+  tags = {
+    Name = "Three-tier-cloud"
+  }
 }
