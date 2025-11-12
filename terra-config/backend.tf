af8905bbe65bce0aae02a21c866e1105e9331bfc
Priya Salunkhe <77096978+Kaustubh-tech17@users.noreply.github.com>
2025-11-12 19:47:50 +0530

    Update backend.tf

--- terra-config/backend.tf
+++ terra-config/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "priya-terra-state-bucket"  # Change if the name already exists. 
+    bucket         = "mypriya-terra-state-bucket"  # Change if the name already exists. 
     key            = "eks/terraform.tfstate"       
     region         = "us-east-1"                   
     encrypt        = true
