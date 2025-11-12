15d3fd7481671cf3166d42d026f8e155d2fdd6c4
Priya Salunkhe <77096978+Kaustubh-tech17@users.noreply.github.com>
2025-11-12 20:56:10 +0530

    Update vpc.tf

--- terra-config/vpc.tf
+++ terra-config/vpc.tf
@@ -1,12 +1,46 @@
 # Get Default VPC
+#data "aws_vpc" "default" {
+#  default = true
+#}
+
+# Get default Subnet ids
+#data "aws_subnets" "public" {
+#  filter {
+#    name   = "vpc-id"
+#    values = [data.aws_vpc.default.id]
+#  }
+#}
+
+###########################################
+# Get Default VPC
+###########################################
 data "aws_vpc" "default" {
   default = true
 }
 
-# Get default Subnet ids
+###########################################
+# Get Default Subnets — filter only supported AZs for EKS
+###########################################
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+
+  # EKS supports only these zones in us-east-1
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  }
+}
+
+###########################################
+# Output for debugging / reference
+###########################################
+output "vpc_id" {
+  value = data.aws_vpc.default.id
+}
+
+output "public_subnet_ids" {
+  value = data.aws_subnets.public.ids
 }
