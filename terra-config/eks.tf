175e17074b5ff2dfc7b75ec687f18f2e86838c89
Priya Salunkhe <77096978+Kaustubh-tech17@users.noreply.github.com>
2025-11-12 21:02:00 +0530

    Update eks.tf

--- terra-config/eks.tf
+++ terra-config/eks.tf
@@ -1,7 +1,7 @@
 
 # Create the EKS Cluster
 #resource "aws_eks_cluster" "eks_cluster" {
-#  name = "Three-tier-cloud"
+#name = "Three-tier-cloud"
 
  # role_arn = aws_iam_role.cluster_role.arn
 
@@ -14,7 +14,7 @@
 #}
 ------------------------------------------------------------------
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "Three-tier-cloud"
+  name = "Three-tier-cloud"
   role_arn = aws_iam_role.cluster_role.arn
 
   vpc_config {
